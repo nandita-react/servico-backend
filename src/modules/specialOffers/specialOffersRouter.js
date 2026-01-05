4cd38a0fcf585c695c912f1b75035ca9ab8c0113
Nandita Maity <maity.nandita2013@gmail.com>
2026-01-05 17:44:57 +0530

    update

--- src/modules/specialOffers/specialOffersRouter.js
+++ src/modules/specialOffers/specialOffersRouter.js
@@ -1,18 +1,46 @@
 const express=require("express");
 const routers=express.Router();
-const offersController=require("./specialOffersController");
+const specialOffersController=require("./specialOffersController");
 
 
 /**
  * @swagger
- * /api/offers:
+ * /api/specialOffers:
  *   get:
- *     summary: Get all offers
- *     tags: [Offers]
+ *     summary: Get all specialOffers
+ *     tags: [specialOffers]
+ *     description: Fetch all promotional specialOffers with banner details
  *     responses:
  *       200:
- *         description: Offers fetched successfully
+ *         description: specialOffers fetched successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                   title:
+ *                     type: string
+ *                   subtitle:
+ *                     type: string
+ *                   description:
+ *                     type: string
+ *                   bgColor:
+ *                     type: string
+ *                     example: "#FF5733"
+ *                   image:
+ *                     type: string
+ *                     description: Image URL
+ *                   cta:
+ *                     type: string
+ *                     example: "Book Now"
+ *                   link:
+ *                     type: string
+ *                     description: Redirect URL
  */
-routers.get("/",offersController.findAll);
+routers.get("/",specialOffersController.findAll);
 
 module.exports=routers
