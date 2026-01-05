75b05bda2ae72f8153af102d3295b3d3b06795b3
Nandita Maity <maity.nandita2013@gmail.com>
2026-01-05 17:40:31 +0530

    update

--- src/modules/setting/settingRouter.js
+++ src/modules/setting/settingRouter.js
@@ -5,29 +5,28 @@ const settingController=require("./settingController");
 
 /**
  * @swagger
- * /api/setting/create:
+ * /api/settings/create:
  *   post:
  *     summary: Create setting
  *     tags: [Setting]
+ *     description: Create application settings with selected city
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
  *             type: object
+ *             required:
+ *               - generalSetting
  *             properties:
  *               generalSetting:
  *                 type: object
+ *                 required:
+ *                   - cityId
  *                 properties:
  *                   cityId:
  *                     type: string
- *               notifications:
- *                 type: object
- *                 properties:
- *                   email:
- *                     type: boolean
- *                   sms:
- *                     type: boolean
+ *                     description: City ID reference
  *     responses:
  *       201:
  *         description: Setting created successfully
@@ -36,13 +35,29 @@ routers.post("/create",settingController.create);
 
 /**
  * @swagger
- * /api/setting:
+ * /api/settings:
  *   get:
  *     summary: Get all settings
  *     tags: [Setting]
+ *     description: Fetch all application settings
  *     responses:
  *       200:
  *         description: Settings fetched successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                   generalSetting:
+ *                     type: object
+ *                     properties:
+ *                       cityId:
+ *                         type: string
+ *                         description: City ID reference
  */
 routers.get("/",settingController.findAll);
 
